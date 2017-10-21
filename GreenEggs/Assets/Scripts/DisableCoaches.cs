483b8416e0684c423ff1fcff2f36bf9a9356806d
Alfonso Morales <hello@teamlaunchable.com>
2017-10-21 01:37:04 -0400

    UI enhancements

---
+++ GreenEggs/Assets/Scripts/DisableCoaches.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCoaches : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        if (PlayerPrefs.GetInt("hasTCDisplayed") == 1)
+        {
+            Destroy(GameObject.Find("Coach"));
+        }
+        PlayerPrefs.SetInt("hasTCDisplayed", 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
