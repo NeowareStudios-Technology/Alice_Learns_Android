313b87591ad35924f491c99d7aad0714dcbb1860
Max Sobhani <msobhani97@outlook.com>
2017-12-06 10:28:49 -0600

    Screen Pofile & SO

---
+++ Alice_Tablet-df0ce0f32177875b5a78adf42021722a9812a32d/GreenEggs/Assets/Orientation/Orientation_Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientation_Manager : MonoBehaviour {
+
+	void Update () {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+	}
+}
